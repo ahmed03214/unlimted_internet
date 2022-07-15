f1791f101c0e777461cb674270d6ceac5fc44f55
ahmed03214 <109301629+ahmed03214@users.noreply.github.com>
2022-07-16 00:03:13 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,36 +6,7 @@ import axios from "axios";
 const _API = process.env.API_URL
 
 const Home = () => {
-  const [success, setSuccess] = useState(null);
-  const [error, setError] = useState(null);
-  const [querys, setQuerys] = useState({});
   const { query } = useRouter();
-
-  useEffect(() => {
-    if (!Object.keys(query)?.length) return;
-
-    const data = Object.fromEntries(new URLSearchParams(query));
-
-    setQuerys(data);
-  }, [query]);
-
-  useEffect(() => {
-    if (!Object.keys(querys)?.length) return;
-
-    axios
-      .post(`${_API}/add-user`, {
-        oauth_token: querys.oauth_token,
-        oauth_token_secret: querys.oauth_token_secret,
-        user_id: querys.user_id,
-      }) //
-      .then((data) => {
-        if(data.code === 200) {
-          return setSuccess(true);
-        };
-      
-        setError({ isError: true, message: data?.message || "unknown error" })
-      });
-  }, [querys]);
 
   return (
     <>
@@ -44,15 +15,9 @@ const Home = () => {
     </Head>
     
     <article className="bg-dark text-light w-100 vh-100 pb-3">
-      {success && (
+      {query?.success && (
         <p className="bg-success text-light d-block mx-auto mb-3 p-2 rounded w-fit">
           تم اضافة الحساب قم بأضافة حساب اخر
-        </p>
-      )}
-
-      {error?.isError && (
-        <p className="bg-danger text-light d-block mx-auto mb-3 p-2 rounded w-fit">
-          {error.message}
         </p>
       )}
 
